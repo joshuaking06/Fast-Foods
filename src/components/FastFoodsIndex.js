e70de3b58c323d6b6b9189871559c83046008014
joshuaking06 <joshua.king0692@gmail.com>
2019-02-04 16:06:38 +0000

    logging data correctly on index page

--- src/components/FastFoodsIndex.js
+++ src/components/FastFoodsIndex.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import axios from 'axios'
 
-const FastFoodsIndex = () => {
-  axios.get('/api/fast-foods')
-    .then(res => console.log(res))
-  return(
-    <h1> Index Page </h1>
-  )
+class FastFoodsIndex extends React.Component{
+  componentDidMount(){
+    axios.get('/api/fast-foods')
+      .then(res => this.setState({ restaurants: res.data }))
+  }
+
+
+  render(){
+    if(!this.state) return null
+    console.log(this.state.restaurants)
+    return(
+      <h1> Check log </h1>
+    )
+  }
 }
 
 
